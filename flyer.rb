6961d85dd83031406b50be64ac526b3bfb26cd3b
George Ayoub <149275120+g6ayoub@users.noreply.github.com>
2025-03-25 03:27:27 +0000

    Improved output line spacing

--- flyer.rb
+++ flyer.rb
@@ -3,12 +3,18 @@ require "./vendor.rb"
 def generate_flyer
 puts "Come see us at the 25th annual Roger's Park Artisan Market!"
 puts "
-
-              IMAGE
+_______________________________________
+|                                     |
+|                                     |
+|              IMAGE                  |
+|                                     |
+|                                     |
+|_____________________________________|
 
 "
 puts "Check out our amazing collection of #{@products}!"
-puts "July 25th @ 1234 Chicago Ave"
-puts "and visit us online at: \n
-Instagram: #{social_sites[0]}, Twitter: #{social_sites[1]}, TikTok: #{social_sites[2]}, Facebook: #{social_sites[3]}, Website: #{social_sites[4]}"
+puts "\nJuly 25th @ 1234 Chicago Ave"
+puts "\nand visit us online at: \n
+Website: #{social_sites[4]} \n
+Instagram: #{social_sites[0]}, Twitter: #{social_sites[1]}, TikTok: #{social_sites[2]}, Facebook: #{social_sites[3]}"
 end
--- vendor.rb
+++ vendor.rb
@@ -15,37 +15,39 @@ class Vendor
     answer = gets.chomp 
     case answer
     when "y"
-      puts "Great! Let's get started."
+      puts "\nGreat! Let's get started."
       register 
     when "n"
-      puts "We hope to see you there!"
+      puts "\nWe hope to see you there!"
     else
-      puts "Sorry, we didn't get that, please type 'y' to register or 'n' to exit!"
+      puts "\nSorry, we didn't get that, please type 'y' to register or 'n' to exit!"
       registration
     end
   end
   
   def register 
-    puts "What is your name?"
+    puts "\nWhat is your name?"
     @your_name = gets.capitalize.chomp
-    puts "Welcome, #{@your_name}!\n"
-    puts "What is the name of your business?"
+    puts "\nWelcome, #{@your_name}!\n"
+    puts "\nWhat is the name of your business?"
     @business_name = gets.chomp
-    puts "Amazing - we're excited to host #{@business_name}, #{@your_name}!\n"
-    puts "What types of products will you be showcasing?"
+    puts "\nAmazing - we're excited to host #{@business_name}, #{@your_name}!\n"
+    puts "\nWhat types of products will you be showcasing?"
     @products = gets.capitalize.chomp
-    puts "Wonderful! We're excited to check out #{@business_name}'s #{@products}!"
+    puts "\nWonderful! We're excited to check out #{@business_name}'s #{@products}!"
     generate_id
   end
 
   def generate_id
     @registration_id = rand(10_000..20_000)
+    puts "\n"
     puts "Your registration id # is #{registration_id}"
     puts "Please keep this id number, in case you have any questions or would like to withdraw from the market."
     marketing
   end
 
   def marketing
+    puts "\n"
    puts "Please enter #{@business_name}'s social media handle(s)or website for our marketing materials! You can skip if you would like by pressing 'Enter' or 'Return' on your device."
     puts "Instagram:"
     instagram = gets.chomp
@@ -57,6 +59,7 @@ class Vendor
     facebook = gets.chomp
     puts "Website:"
     website = gets.chomp
+    puts "\n"
     puts "Thank you! We have automatically generated this flyer for you to share:"
     puts "\n"
 
