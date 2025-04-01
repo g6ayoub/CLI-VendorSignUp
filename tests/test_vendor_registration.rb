116aef0bf40d1953badf7476c855a55496f61c50
George Ayoub <149275120+g6ayoub@users.noreply.github.com>
2025-04-01 18:23:38 +0000

    Added de-bugging test for email collection method

--- tests/test_vendor_registration.rb
+++ tests/test_vendor_registration.rb
@@ -2,4 +2,8 @@ require 'minitest/autorun'
 require './app'
 
 class TestRegistration < Minitest::Test 
-  def test_registration
+  def test_registration()
+    new_vendor = Vendor.new
+    assert_equal "jim@gmail.com", new_vendor.collect_email("jim@gmail.com"), "Email collection method failed"
+  end
+end
