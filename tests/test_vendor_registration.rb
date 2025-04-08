f351eb8554e9c0bc04258e6eaf89cbc98dc0b72e
George Ayoub <149275120+g6ayoub@users.noreply.github.com>
2025-04-08 18:10:00 +0000

    Added a test method for user info collection

--- tests/test_vendor_registration.rb
+++ tests/test_vendor_registration.rb
@@ -2,8 +2,21 @@ require 'minitest/autorun'
 require './app'
 
 class TestRegistration < Minitest::Test 
-  def test_registration()
-    new_vendor = Vendor.new
-    assert_equal "jim@gmail.com", new_vendor.collect_email("jim@gmail.com"), "Email collection method failed"
+  def test_collect_email
+    vendor = Vendor.new
+    
+    fake_input = StringIO.new("test@example.com\n")
+    original_stdin = $stdin
+    $stidin = fake_input
+
+    vendor.collect_email
+
+    $stdin = original_stdin
+    assert_equal "test@example.com", vendor.email
   end
 end
+   
+   
+   # assert_equal "jim@gmail.com", new_vendor.collect_email("jim@gmail.com"), "Email collection method failed"
+ # end
+#end
